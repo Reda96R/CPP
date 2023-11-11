09b78d551426e31f0cef5e2e91916b2b73e9d772
Reda96R <redarayyad@yahoo.com>
2023-11-11 15:28:23 +0100

    finished ex01

--- CPP_01/ex00/main.cpp
+++ CPP_01/ex00/main.cpp
@@ -1,17 +1,18 @@
 #include "Zombie.hpp"
 #include <cstdlib>
+
 int	main(){
 	std::string	Z;
 	
 	system("clear");
-	std::cout << "\033[0;32mDeploying Zombie on the heap\033[0m" << std::endl;
+	std::cout << "\033[0;32mDeploying Zombie on the heap 🧟\033[0m" << std::endl;
 	std::cout << "Enter the Zombie's name: " << std::flush;
 	std::cin >> Z;
-	Zombie *Z2 = newZombie(Z);
-	Z2->announce();
-	delete Z2;
+	Zombie *Z1 = newZombie(Z);
+	Z1->announce();
+	delete Z1;
 	
-	std::cout << "\033[0;32mCalling randomChump\033[0m" << std::endl;
+	std::cout << "\033[0;32m\nDeploying a Zombie on the stack 🧟\033[0m" << std::endl;
 	std::cout << "Enter the Zombie's name: " << std::flush;
 	std::cin >> Z;
 	randomChump(Z);
