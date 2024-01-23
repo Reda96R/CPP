41dbec1d869f8a7997abe2eae97afa36f655089b
Reda96R <redarayyad@yahoo.com>
2024-01-23 18:13:58 +0100

    implementing the form class

---
+++ CPP_05/ex01/Form.hpp
@@ -0,0 +1,37 @@
+#ifndef FORM_HPP
+# define FORM_HPP
+
+# include "Bureaucrat.hpp"
+
+class Form{
+	private:
+		const std::string name;
+		bool			  _signed;
+		const int		  gradeToSign;
+		const int		  grdaeToExecute;
+
+	public:
+		Form( void );
+		Form( const std::string& name, int gradeToSign );
+		Form( const std::string& name, int gradeToSign , int grdaeToExecute );
+		Form( const Form& src );
+		Form& operator=( const Form& rhs );
+		~Form( void );
+		
+		class GradeTooLowException : public std::exception{
+			public:
+				virtual const char*	what() const throw(){
+					return ("\033[0;31mGrade too low\033[0m");
+				}
+		};
+		class GradeTooHighException : public std::exception{
+			public:
+				virtual const char*	what() const throw(){
+					return ("\033[0;31mGrade too high\033[0m");
+				}
+		};
+};
+
+std::ostream&	operator<<(std::ostream& o, const Form& rhs);
+
+#endif
